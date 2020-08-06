ed7dd92d7b34b55389ca4a89e0b022375be6ae5a
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-06 19:47:39 -0300

    Create tabs navigation

---
+++ src/routes/StudyTabs.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import TeacherList from '../pages/TeacherList';
+import Favorites from '../pages/Favorites';
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+const StudyTabs: React.FC = () => {
+  return (
+    <Navigator
+      tabBarOptions={{
+        style: {
+          elevation: 0,
+          height: 64,
+          shadowOpacity: 0,
+        },
+        tabStyle: {
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'center',
+        },
+        iconStyle: {
+          flex: 0,
+          height: 20,
+          width: 20,
+        },
+        labelStyle: {
+          fontFamily: 'Archivo_700Bold',
+          fontSize: 13,
+          marginLeft: 16,
+        },
+        inactiveBackgroundColor: '#fafafc',
+        activeBackgroundColor: '#ebebf5',
+        inactiveTintColor: '#c1bccc',
+        activeTintColor: '#32264d',
+      }}
+    >
+      <Screen
+        name="TeacherList"
+        component={TeacherList}
+        options={{
+          tabBarLabel: 'Proffys',
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-easel"
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
+            );
+          },
+        }}
+      />
+      <Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          tabBarLabel: 'Favoritos',
+          tabBarIcon: ({ color, size, focused }) => {
+            return (
+              <Ionicons
+                name="ios-heart"
+                size={size}
+                color={focused ? '#8257e5' : color}
+              />
+            );
+          },
+        }}
+      />
+    </Navigator>
+  );
+};
+
+export default StudyTabs;
