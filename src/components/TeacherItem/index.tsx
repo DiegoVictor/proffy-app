bbc430749508267a0a29b5d990d37f781f24ddfc
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-08 09:25:22 -0300

    Create teacher component

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,120 @@
+import React, { useCallback, useState } from 'react';
+import { Image, Linking, Alert } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import HeartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import UnfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import WhatsAppIcon from '../../assets/images/icons/whatsapp.png';
+import formatValue from '../../utils/formatValue';
+import {
+  Container,
+  Profile,
+  Avatar,
+  ProfileInfo,
+  Name,
+  Subject,
+  Bio,
+  Footer,
+  Price,
+  PriveValue,
+  ButtonsContainer,
+  FavoriteButton,
+  ContactButton,
+  ContactButtonText,
+} from './styles';
+import api from '../../services/api';
+
+export interface Teacher {
+  id: number;
+  subject: string;
+  cost: number;
+  name: string;
+  avatar: string;
+  whatsapp: string;
+  bio: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+  favorited: boolean;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher, favorited }) => {
+  const [isFavorited, setIsFavorited] = useState(favorited);
+  const handleLinkToWhatsApp = useCallback(() => {
+    try {
+      api.post('connections', { user_id: teacher.id });
+      Linking.openURL(`whatsapp://send?phone=${teacher.whatsapp}`);
+    } catch (err) {
+      Alert.alert('Ops! Alguma coisa deu errado, tente novamente!');
+    }
+  }, []);
+
+  const handleToggleFavorite = useCallback(async () => {
+    const storedFavorites = await AsyncStorage.getItem('favorites');
+    const favorites = [];
+
+    if (storedFavorites) {
+      favorites.push(...JSON.parse(storedFavorites));
+    }
+
+    if (isFavorited) {
+      const favoriteIndex = favorites.findIndex(
+        (teacherItem: Teacher) => teacherItem.id === teacher.id,
+      );
+      favorites.splice(favoriteIndex, 1);
+      setIsFavorited(false);
+    } else {
+      favorites.push(teacher);
+      setIsFavorited(true);
+    }
+
+    await AsyncStorage.setItem('favorites', JSON.stringify(favorites));
+  }, []);
+
+  return (
+    <Container>
+      <Profile>
+        <Avatar
+          source={{
+            uri: teacher.avatar,
+          }}
+        />
+
+        <ProfileInfo>
+          <Name>{teacher.name}</Name>
+          <Subject>{teacher.subject}</Subject>
+        </ProfileInfo>
+      </Profile>
+
+      <Bio>{teacher.bio}</Bio>
+
+      <Footer>
+        <Price>
+          Preço/hora {'   '}
+          <PriveValue>{formatValue(teacher.cost)}</PriveValue>
+        </Price>
+
+        <ButtonsContainer>
+          <FavoriteButton
+            favorited={isFavorited}
+            onPress={handleToggleFavorite}
+          >
+            {isFavorited ? (
+              <Image source={UnfavoriteIcon} />
+            ) : (
+              <Image source={HeartOutlineIcon} />
+            )}
+          </FavoriteButton>
+
+          <ContactButton onPress={handleLinkToWhatsApp}>
+            <Image source={WhatsAppIcon} />
+            <ContactButtonText>Entrar em contato</ContactButtonText>
+          </ContactButton>
+        </ButtonsContainer>
+      </Footer>
+    </Container>
+  );
+};
+
+export default TeacherItem;
---
+++ src/components/TeacherItem/styles.ts
@@ -0,0 +1,105 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+interface FavoriteButtonProps {
+  favorited: boolean;
+}
+
+export const Container = styled.View`
+  background-color: #fff;
+  border-color: #e6e6f0;
+  border-radius: 8px;
+  border-width: 1px;
+  margin-bottom: 16px;
+  overflow: hidden;
+`;
+
+export const Profile = styled.View`
+  align-items: center;
+  flex-direction: row;
+  padding: 24px;
+`;
+
+export const Avatar = styled.Image`
+  background-color: #eee;
+  border-radius: 32px;
+  height: 64px;
+  width: 64px;
+`;
+
+export const ProfileInfo = styled.View`
+  margin-left: 16px;
+`;
+
+export const Name = styled.Text`
+  color: #32264d;
+  font-family: 'Archivo_700Bold';
+  font-size: 20px;
+`;
+
+export const Subject = styled.Text`
+  color: #6a6180;
+  font-family: 'Poppins_400Regular';
+  font-size: 12px;
+  margin-top: 4px;
+`;
+
+export const Bio = styled.Text`
+  color: #6a6180;
+  font-family: 'Poppins_400Regular';
+  font-size: 14px;
+  line-height: 24px;
+  margin: 0px 24px;
+`;
+
+export const Footer = styled.View`
+  align-items: center;
+  background-color: #fafafc;
+  margin-top: 24px;
+  padding: 24px;
+`;
+
+export const Price = styled.Text`
+  color: #6a6180;
+  font-family: 'Poppins_400Regular';
+  font-size: 14px;
+`;
+
+export const PriveValue = styled.Text`
+  color: #8257e5;
+  font-family: 'Archivo_700Bold';
+  font-size: 16px;
+`;
+
+export const ButtonsContainer = styled.View`
+  flex-direction: row;
+  margin-top: 16px;
+`;
+
+export const FavoriteButton = styled(RectButton)<FavoriteButtonProps>`
+  align-items: center;
+  background-color: ${props => (props.favorited ? '#e33d3d' : '#8257e5')};
+  border-radius: 8px;
+  justify-content: center;
+  height: 56px;
+  margin-right: 8px;
+  width: 56px;
+`;
+
+export const ContactButton = styled(RectButton)`
+  align-items: center;
+  background-color: #04d361;
+  border-radius: 8px;
+  flex: 1;
+  flex-direction: row;
+  justify-content: center;
+  height: 56px;
+  margin-right: 8px;
+`;
+
+export const ContactButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Archivo_700Bold';
+  font-size: 16px;
+  margin-left: 16px;
+`;
