a59ab335c824a9d03c9a1f7145055f2191dbf8e2
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-08-08 09:25:53 -0300

    Show available teachers

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,13 +1,120 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useCallback, useRef } from 'react';
+import { Feather } from '@expo/vector-icons';
+import { Alert } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import AsyncStorage from '@react-native-community/async-storage';
+import { FormHandles } from '@unform/core';
 
-import { Container } from './styles';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import api from '../../services/api';
+import { Teacher } from '../../components/TeacherItem';
+import {
+  Container,
+  List,
+  SearchForm,
+  Label,
+  InputGroup,
+  InputBlock,
+  SubmitButton,
+  SubmitButtonText,
+} from './styles';
+import { useFocusEffect } from '@react-navigation/native';
 
 const TeacherList: React.FC = () => {
+  const [teachers, setTeachers] = useState<Teacher[]>([]);
+  const [favorites, setFavorites] = useState<number[]>([]);
+  const [isFiltersVisible, setIsfiltersVisible] = useState(false);
+  const formRef = useRef<FormHandles>(null);
+
+  const handleToggleFiltersVisible = useCallback(() => {
+    setIsfiltersVisible(!isFiltersVisible);
+  }, [isFiltersVisible]);
+
+  const handleSubmit = useCallback(async ({ subject, week_day, time }) => {
+    try {
+      const { data } = await api.get<Teacher[]>('classes', {
+        params: {
+          subject: subject.trim(),
+          week_day: week_day.trim(),
+          time: time.trim(),
+        },
+      });
+
+      setTeachers(data);
+      setIsfiltersVisible(false);
+      loadFavorites();
+    } catch (err) {
+      Alert.alert('Ops! Alguma coisa deu errado, tente mais tarde!');
+    }
+  }, []);
+
+  const loadFavorites = useCallback(() => {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favorited = JSON.parse(response);
+        setFavorites(favorited.map((teacher: Teacher) => teacher.id));
+      }
+    });
+  }, []);
+
+  useFocusEffect(() => {
+    loadFavorites();
+  });
+
   return (
     <Container>
-      <PageHeader title="Proffys disponíveis" />
+      <PageHeader
+        title="Proffys disponíveis"
+        headerRight={
+          <BorderlessButton onPress={handleToggleFiltersVisible}>
+            <Feather name="filter" size={20} color="#FFF" />
+          </BorderlessButton>
+        }
+      >
+        {isFiltersVisible && (
+          <SearchForm ref={formRef} onSubmit={handleSubmit}>
+            <Label>Máteria</Label>
+            <Input name="subject" placeholder="Qual a máteria?" />
+
+            <InputGroup>
+              <InputBlock>
+                <Label>Dia da semana</Label>
+                <Input name="week_day" placeholder="Qual o dia?" />
+              </InputBlock>
+
+              <InputBlock>
+                <Label>Horário</Label>
+                <Input name="time" placeholder="Qual o horário?" />
+              </InputBlock>
+            </InputGroup>
+
+            <SubmitButton
+              onPress={() => {
+                formRef.current?.submitForm();
+              }}
+            >
+              <SubmitButtonText>Filtrar</SubmitButtonText>
+            </SubmitButton>
+          </SearchForm>
+        )}
+      </PageHeader>
+
+      <List
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        {teachers.map((teacher: Teacher) => (
+          <TeacherItem
+            key={teacher.id}
+            teacher={teacher}
+            favorited={favorites.includes(teacher.id)}
+          />
+        ))}
+      </List>
     </Container>
   );
 };
--- src/pages/TeacherList/styles.ts
+++ src/pages/TeacherList/styles.ts
@@ -1,6 +1,45 @@
 import styled from 'styled-components/native';
+import { Form } from '@unform/mobile';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   background-color: #f0f0f7;
   flex: 1;
+`;
+
+export const List = styled.ScrollView`
+  margin-top: -40px;
+`;
+
+export const SearchForm = styled(Form)`
+  margin-bottom: 24px;
+`;
+
+export const Label = styled.Text`
+  color: #d4c2ff;
+  font-family: 'Poppins_400Regular';
+`;
+
+export const InputGroup = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InputBlock = styled.View`
+  width: 48%;
+`;
+
+export const SubmitButton = styled(RectButton)`
+  align-items: center;
+  background-color: #04d361;
+  border-radius: 8px;
+  flex-direction: row;
+  justify-content: center;
+  height: 56px;
+`;
+
+export const SubmitButtonText = styled.Text`
+  color: #fff;
+  font-family: 'Archivo_700Bold';
+  font-size: 16px;
 `;
