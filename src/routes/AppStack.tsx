5543b810332f8727f90120798955932ac5c2ee1c
Diego Victor <diegovictorgonzaga@gmail.com>
2023-10-09 10:51:50 -0300

    Use native stack and set initial route name

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
 import StudyTabs from './StudyTabs';
 
-const { Navigator, Screen } = createStackNavigator();
+export type StackParamList = {
+  Landing: undefined;
+  GiveClasses: undefined;
+  Study: undefined;
+};
+
+const { Navigator, Screen } = createNativeStackNavigator<StackParamList>();
 
 const AppStack: React.FC = () => {
   return (
     <NavigationContainer>
-      <Navigator screenOptions={{ headerShown: false }}>
+      <Navigator
+        screenOptions={{ headerShown: false }}
+        initialRouteName="Landing"
+      >
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
         <Screen name="Study" component={StudyTabs} />
