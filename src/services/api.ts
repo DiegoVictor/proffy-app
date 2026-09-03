ad82b9b08196e5ad5db6f5fdbb5676728ef39fb7
Diego Victor <diegovictorgonzaga@gmail.com>
2026-09-03 09:25:49 -0300

    Update API base URL to use environment variable

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { API_URL } from '@env';
 
-export default axios.create({
-  baseURL: API_URL,
+export const api = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
 });
