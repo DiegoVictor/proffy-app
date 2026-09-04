8fbe2f0093b3a082ba1261caafdf2234fbcdf3bc
Diego Victor <diegovictorgonzaga@gmail.com>
2026-09-04 08:54:40 -0300

    Refactor Input component to simplify structure and remove Unform integration

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,57 +1,21 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { TextInputProps } from 'react-native';
-import { useField } from '@unform/core';
-
 import { TextInput, Error } from './styles';
 
 interface InputProps extends TextInputProps {
   name: string;
+  error?: string;
 }
 
-const Input: React.FC<InputProps> = ({ name, ...rest }) => {
-  const inputRef = useRef<any>(null);
-
-  const { fieldName, registerField, defaultValue, error } = useField(name);
-
-  useEffect(() => {
-    inputRef.current.value = defaultValue;
-  }, [defaultValue]);
-
-  useEffect(() => {
-    registerField<string>({
-      name: fieldName,
-      ref: inputRef.current,
-      clearValue(ref) {
-        ref.value = '';
-        ref.clear();
-      },
-      setValue(ref, value) {
-        ref.setNativeProps({ text: value });
-        inputRef.current.value = value;
-      },
-      getValue(ref) {
-        return ref.value;
-      },
-    });
-  }, [fieldName, registerField]);
-
+export const Input: React.FC<InputProps> = ({ error, ...props }) => {
   return (
     <>
       <TextInput
-        ref={inputRef}
         keyboardAppearance="dark"
-        defaultValue={defaultValue}
         placeholderTextColor="#c1bccc"
-        onChangeText={value => {
-          if (inputRef.current) {
-            inputRef.current.value = value;
-          }
-        }}
-        {...rest}
+        {...props}
       />
       {error && <Error>{error}</Error>}
     </>
   );
 };
-
-export { Input };
